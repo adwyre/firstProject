146955f657bdbbbe28d9e11f3c83c38140f622fa
adwyre <44531434+adwyre@users.noreply.github.com>
2018-11-24 14:14:09 -0500

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -5,20 +5,26 @@ var guess;
 var guessCount = 0;
 var hitCount = 0;
 var isSunk = false;
+
 while (isSunk == false) {
   guess = prompt("Enter number 0-6:");
-  if (guess < 0 || > 6) {
+  if (guess < 0 || guess > 6) {
     alert("Enter valid number 0-6");
-  } else if (guess == location1 || location2 || location3) {
+  } else {
     guessCount += 1;
+  
+    if (guess == location1 || guess == location2 || guess == location3) {
     hitCount += 1;
-    alert "hit";
-  } else (guess !== location1 || location2 || location3){
-    guessCount += 1;
-    alert "miss";
-  }
-if (hitCount == 3){
-  isSunk = true;
-  alert "You sunk my battleship! Guesses: + 'guessCount'";
+    alert ("hit");
+      if (hitCount == 3){
+      isSunk = true;
+      alert ("You sunk my battleship!");
+      } 
+    } else {
+    alert ("miss");
+    }
+  }  
 }
-}
+var stats = "You took " + guessCount + "guesses to sink the battleship, whic means your shooting accuracy was " +
+    (3/guessCount);
+alert (stats);
